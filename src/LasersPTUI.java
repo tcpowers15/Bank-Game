cd3caed60cb14b733bddbee2706669e0932d6ed1
Brian <brp6671@rit.edu>
2016-04-08 18:21:22 -0400

    Initial Revision

--- src/LasersPTUI.java
+++ src/LasersPTUI.java
@@ -1,11 +1,9 @@
-/**
- * Created by BPow11 on 4/8/16.
- */
+
 public class LasersPTUI {
 
     public static void main(String[] args) {
         System.out.println("My name is Brian Powers");
         System.out.println("My name is Trevor Powers");
-        System.out.println("Our project account is p142-11h");
+        System.out.println("Our Project account is p142-11h");
     }
 }
