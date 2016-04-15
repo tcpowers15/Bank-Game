2f2e1fb1f9fd082cc3ac65c71b81b383e99c985f
Brian <brp6671@rit.edu>
2016-04-15 18:08:40 -0400

    Remove finally completed

--- src/board.java
+++ src/board.java
@@ -179,7 +179,7 @@ public class board {
     public void add(int r, int c) {
         if (board[r][c] == LASER || board[r][c] == xPillar || board[r][c] == zero || board[r][c] == one
                 || board[r][c] == two || board[r][c] == three || board[r][c] == four) {
-            System.out.println("Error adding laser at: (" + r + "," + c + ")");
+            System.out.println("Error adding laser at: (" + r + ", " + c + ")");
         }
         else if(board[r][c] == EMPTY || board[r][c] == L_BEAM){
             board[r][c] = LASER;
@@ -192,7 +192,7 @@ public class board {
                 }
 
             }
-            for(int j = 1; r-j > 0; j++){
+            for(int j = 1; r-j >= 0; j++){
                 if(board[r-j][c] == EMPTY || board[r-j][c] == L_BEAM){
                     board[r-j][c] = L_BEAM;
                 }
@@ -208,7 +208,7 @@ public class board {
                     break;
                 }
             }
-            for(int l = 1; c-l > 0; l++){
+            for(int l = 1; c-l >= 0; l++){
                 if(board[r][c-l] == EMPTY || board[r][c-l] == L_BEAM){
                     board[r][c-l] = L_BEAM;
                 }
@@ -216,6 +216,7 @@ public class board {
                     break;
                 }
             }
+            System.out.println("Laser added at: (" + r + ", " + c + ")");
         }
     }
 
@@ -225,20 +226,27 @@ public class board {
      * This method removes a laser from a given position
      * @param r the row number
      * @param c the column number
-     */
+
     public void remove(int r, int c){
         boolean hasLaser;
         boolean hasLaser2;
         boolean hasLaser3;
+        boolean column;
         if (board[r][c] == LASER){
             hasLaser = false;
             hasLaser2 = false;
             hasLaser3 = false;
+            column = false;
             for(int i = 1; r+i < numRows;i++){
                 if(board[r+i][c] == L_BEAM && !hasLaser){
                     for(int m = 1; r+m < numRows; m++){
                         if(board[r+m][c] == LASER){
                             hasLaser = true;
+                            break;
+                        }
+                        else if(board[r+m][c] == xPillar || board[r+m][c] == zero || board[r+m][c] == one ||
+                                board[r+m][c] == two || board[r+m][c] == three || board[r+m][c] == four){
+                            hasLaser2 = true;
                             break;
                         }
                         else{
@@ -250,12 +258,22 @@ public class board {
                             hasLaser2 = true;
                             break;
                         }
+                        else if(board[r][c+n] == xPillar || board[r][c+n] == zero || board[r][c+n] == one ||
+                                board[r][c+n] == two || board[r][c+n] == three || board[r][c+n] == four){
+                            hasLaser2 = true;
+                            break;
+                        }
                         else{
                             hasLaser2 = false;
                         }
                     }
-                    for(int o = 1; c-o > 0; o++){
+                    for(int o = 1; c-o >= 0; o++){
                         if(board[r][c-o] == LASER){
+                            hasLaser3 = true;
+                            break;
+                        }
+                        else if(board[r][c-o] == xPillar || board[r][c-o] == zero || board[r][c-o] == one ||
+                                board[r][c-o] == two || board[r][c-o] == three || board[r][c-o] == four){
                             hasLaser3 = true;
                             break;
                         }
@@ -277,11 +295,17 @@ public class board {
             hasLaser = false;
             hasLaser2 = false;
             hasLaser3 = false;
-            for(int j = 1; r-j > 0; j++){
+            column = false;
+            for(int j = 1; r-j >= 0; j++){
                 if(board[r-j][c] == L_BEAM && !hasLaser){
-                    for(int jm = 1; r-jm < numRows; jm++){
+                    for(int jm = 1; r-jm >= 0; jm++){
                         if(board[r-jm][c] == LASER){
                             hasLaser = true;
+                            break;
+                        }
+                        else if(board[r-jm][c] == xPillar || board[r-jm][c] == zero || board[r-jm][c] == one ||
+                                board[r-jm][c] == two || board[r-jm][c] == three || board[r-jm][c] == four){
+                            hasLaser2 = true;
                             break;
                         }
                         else{
@@ -293,12 +317,22 @@ public class board {
                             hasLaser2 = true;
                             break;
                         }
+                        else if(board[r][c+jn] == xPillar || board[r][c+jn] == zero || board[r][c+jn] == one ||
+                                board[r][c+jn] == two || board[r][c+jn] == three || board[r][c+jn] == four){
+                            hasLaser2 = true;
+                            break;
+                        }
                         else{
                             hasLaser2 = false;
                         }
                     }
-                    for(int jo = 1; c-jo > 0; jo++){
+                    for(int jo = 1; c-jo >= 0; jo++){
                         if(board[r][c-jo] == LASER){
+                            hasLaser3 = true;
+                            break;
+                        }
+                        else if(board[r][c-jo] == xPillar || board[r][c-jo] == zero || board[r][c-jo] == one ||
+                                board[r][c-jo] == two || board[r][c-jo] == three || board[r][c-jo] == four){
                             hasLaser3 = true;
                             break;
                         }
@@ -320,11 +354,17 @@ public class board {
             hasLaser = false;
             hasLaser2 = false;
             hasLaser3 = false;
+            column = false;
             for(int k = 1; c+k < numCol; k++){
                 if(board[r][c+k] == L_BEAM && !hasLaser){
                     for(int km = 1; c+km < numRows; km++){
                         if(board[r][c+km] == LASER){
                             hasLaser = true;
+                            break;
+                        }
+                        else if(board[r][c+km] == xPillar || board[r][c+km] == zero || board[r][c+km] == one ||
+                                board[r][c+km] == two || board[r][c+km] == three || board[r][c+km] == four){
+                            hasLaser2 = true;
                             break;
                         }
                         else{
@@ -333,6 +373,11 @@ public class board {
                     }
                     for(int kn = 1; r+kn < numCol; kn++){
                         if(board[r+kn][c] == LASER){
+                            hasLaser2 = false;
+                            break;
+                        }
+                        else if(board[r+kn][c] == xPillar || board[r+kn][c] == zero || board[r+kn][c] == one ||
+                                board[r+kn][c] == two || board[r+kn][c] == three || board[r+kn][c] == four){
                             hasLaser2 = true;
                             break;
                         }
@@ -340,8 +385,13 @@ public class board {
                             hasLaser2 = false;
                         }
                     }
-                    for(int ko = 1; r-ko > 0; ko++){
+                    for(int ko = 1; r-ko >= 0; ko++){
                         if(board[r-ko][c] == LASER){
+                            hasLaser3 = true;
+                            break;
+                        }
+                        else if(board[r-ko][c] == xPillar || board[r-ko][c] == zero || board[r-ko][c] == one ||
+                                board[r-ko][c] == two || board[r-ko][c] == three || board[r-ko][c] == four){
                             hasLaser3 = true;
                             break;
                         }
@@ -363,11 +413,17 @@ public class board {
             hasLaser = false;
             hasLaser2 = false;
             hasLaser3 = false;
-            for(int l = 1; c-l > 0; l++){
+            column = false;
+            for(int l = 1; c-l >= 0; l++){
                 if(board[r][c-l] == L_BEAM && !hasLaser){
-                    for(int lm = 1; c-lm < numRows; lm++){
+                    for(int lm = 1; c-lm >= 0; lm++){
                         if(board[r][c-lm] == LASER){
                             hasLaser = true;
+                            break;
+                        }
+                        else if(board[r][c-lm] == xPillar || board[r][c-lm] == zero || board[r][c-lm] == one ||
+                                board[r][c-lm] == two || board[r][c-lm] == three || board[r][c-lm] == four){
+                            hasLaser2 = false;
                             break;
                         }
                         else{
@@ -379,13 +435,23 @@ public class board {
                             hasLaser2 = true;
                             break;
                         }
+                        else if(board[r+ln][c] == xPillar || board[r+ln][c] == zero || board[r+ln][c] == one ||
+                                board[r+ln][c] == two || board[r+ln][c] == three || board[r+ln][c] == four){
+                            hasLaser2 = false;
+                            break;
+                        }
                         else{
                             hasLaser2 = false;
                         }
                     }
-                    for(int lo = 1; r-lo > 0; lo++){
+                    for(int lo = 1; r-lo >= 0; lo++){
                         if(board[r-lo][c] == LASER){
                             hasLaser3 = true;
+                            break;
+                        }
+                        else if(board[r-lo][c] == xPillar || board[r-lo][c] == zero || board[r-lo][c] == one ||
+                                board[r-lo][c] == two || board[r-lo][c] == three || board[r-lo][c] == four){
+                            hasLaser3 = false;
                             break;
                         }
                         else{
@@ -406,12 +472,13 @@ public class board {
             if(board[r][c] == LASER){
                 board[r][c] = EMPTY;
             }
+            System.out.println("Laser removed at: (" + r + ", " + c + ")");
         }
         else{
-            System.out.println("Error removing laser at: (" + r + "," + c + ")");
+            System.out.println("Error removing laser at: (" + r + ", " + c + ")");
         }
     }
-
+*/
 
 
 
@@ -1272,5 +1339,127 @@ public class board {
         else{
             return false;
         }
+    }
+
+    public void remove(int r, int c){
+        if (board[r][c] == LASER) {
+            if (removedown(r, c) || removeup(r, c)) {
+                board[r][c] = L_BEAM;
+            }
+            else {
+                for (int row = r; row < numRows; row++) {
+                    if (!removeright(row, c) && !removeleft(row, c) && board[row][c] == L_BEAM) {
+                        board[row][c] = EMPTY;
+                    }
+                    else if (board[row][c] == xPillar || board[row][c] == zero || board[row][c] == one ||
+                            board[row][c] == two || board[row][c] == three || board[row][c] == four) {
+                        break;
+                    }
+                }
+                for (int row = r; row >= 0; row--) {
+                    if (!removeright(row, c) && !removeleft(row, c) && board[row][c] == L_BEAM) {
+                        board[row][c] = EMPTY;
+                    }
+                    else if (board[row][c] == xPillar || board[row][c] == zero || board[row][c] == one ||
+                            board[row][c] == two || board[row][c] == three || board[row][c] == four) {
+                        break;
+                    }
+                }
+            }
+            if (removeleft(r, c) || removeright(r, c)) {
+                board[r][c] = L_BEAM;
+            }
+            else {
+                for (int col = c; col < numCol; col++) {
+                    if (!removeup(r, col) && !removedown(r, col) && board[r][col] == L_BEAM) {
+                        board[r][col] = EMPTY;
+                    }
+                    else if (board[r][col] == xPillar || board[r][col] == zero || board[r][col] == one ||
+                            board[r][col] == two || board[r][col] == three || board[r][col] == four) {
+                        break;
+                    }
+                }
+                for (int col = c; col >= 0; col--) {
+                    if (!removeup(r, col) && !removedown(r, col) && board[r][col] == L_BEAM) {
+                        board[r][col] = EMPTY;
+                    }
+                    else if (board[r][col] == xPillar || board[r][col] == zero || board[r][col] == one ||
+                            board[r][col] == two || board[r][col] == three || board[r][col] == four) {
+                        break;
+                    }
+                }
+            }
+            if(board[r][c] == LASER){
+                board[r][c] = EMPTY;
+            }
+            System.out.println("Laser removed at: (" + r + ", " + c + ")");
+        }
+        else{
+            System.out.println("Error removing laser at: (" + r + ", " + c + ")");
+        }
+    }
+
+    public boolean removeright(int r, int c){
+        boolean hasLaserright = false;
+
+        for(int col = c+1; col < numCol; col++) {
+            if (board[r][col] == xPillar || board[r][col] == zero || board[r][col] == one ||
+                    board[r][col] == two || board[r][col] == three || board[r][col] == four) {
+                break;
+            }
+            else if (board[r][col] == LASER) {
+                hasLaserright = true;
+                break;
+            }
+        }
+        return hasLaserright;
+    }
+
+    public boolean removeleft(int r, int c){
+        boolean hasLaserleft = false;
+
+        for(int col = c-1; col >= 0; col--) {
+            if (board[r][col] == xPillar || board[r][col] == zero || board[r][col] == one ||
+                    board[r][col] == two || board[r][col] == three || board[r][col] == four) {
+                break;
+            }
+            else if (board[r][col] == LASER) {
+                hasLaserleft = true;
+                break;
+            }
+        }
+        return hasLaserleft;
+    }
+
+    public boolean removeup(int r, int c){
+        boolean hasLaserup = false;
+
+        for(int row = r+1; row < numRows; row++) {
+            if (board[row][c] == xPillar || board[row][c] == zero || board[row][c] == one ||
+                    board[row][c] == two || board[row][c] == three || board[row][c] == four) {
+                break;
+            }
+            else if (board[row][c] == LASER) {
+                hasLaserup = true;
+                break;
+            }
+        }
+        return hasLaserup;
+    }
+
+    public boolean removedown(int r, int c){
+        boolean hasLaserdown = false;
+
+        for(int row = r-1; row >= 0; row--) {
+            if (board[row][c] == xPillar || board[row][c] == zero || board[row][c] == one ||
+                    board[row][c] == two || board[row][c] == three || board[row][c] == four) {
+                break;
+            }
+            else if (board[row][c] == LASER) {
+                hasLaserdown = true;
+                break;
+            }
+        }
+        return hasLaserdown;
     }
 }
